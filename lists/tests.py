4d43e8fe8ac8d472de844a0061b0963c040e5d2f
Wajih ElKaterji <wajih.elkaterji@wearelayer.com>
2024-04-23 16:19:09 +0300

    Added the homepage mapper and its tests

--- lists/tests.py
+++ lists/tests.py
@@ -3,11 +3,9 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 class HomePage(TestCase):
+
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode("utf8")
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.startswith("<html>"))
-        self.assertTrue(html.endswith("</html>"))
-        
+        response = self.client.get("/")
+        self.assertContains(response, "<title>To-Do lists</title>")
+        self.assertContains(response, "<html>")
+        self.assertContains(response, "</html>")
