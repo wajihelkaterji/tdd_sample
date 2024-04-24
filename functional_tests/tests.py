8ae5428fd5dfaaa8a77262ee64875648d9c735d7
Wajih ElKaterji <wajih.elkaterji@wearelayer.com>
2024-04-24 11:13:04 +0300

    refactored functional tests to remove time.wait

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,12 @@
-import unittest
 import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 from django.test import LiveServerTestCase
 
+MAX_WAIT = 5
 class NewVisitorTest(LiveServerTestCase):  
     def setUp(self):  
         self.browser = webdriver.Firefox()  
@@ -13,11 +14,24 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):  
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn(row_text, [row.text for row in rows])
+    # def check_for_row_in_list_table(self, row_text):
+    #     table = self.browser.find_element(By.ID, "id_list_table")
+    #     rows = table.find_elements(By.TAG_NAME, "tr")
+    #     self.assertIn(row_text, [row.text for row in rows])
 
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, "id_list_table")
+                rows = table.find_elements(By.TAG_NAME, "tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException):
+                if (time.time() - start_time) > MAX_WAIT:
+                    raise
+
+                time.sleep(0.5)
 
     def test_can_start_a_todo_list(self):  
         # Edith has heard about a cool new online to-do app.
@@ -40,9 +54,7 @@ class NewVisitorTest(LiveServerTestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly"
@@ -50,11 +62,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        self.check_for_row_in_list_table("1: Buy peacock feathers")
-        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
+        self.wait_for_row_in_list_table("1: Buy peacock feathers")
+        self.wait_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         self.fail("Finish the test!")  
 
