56ca352a69d68f9166ff93640fcd5cb8f50b1c26
Wajih ElKaterji <wajih.elkaterji@wearelayer.com>
2024-04-24 13:44:28 +0300

    Separated templates for list and home pages and refactores

--- lists/tests.py
+++ lists/tests.py
@@ -8,13 +8,6 @@ class HomePage(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response, "home.html")
-    
-    def test_displays_all_list_items(self):
-        Item.objects.create(text="itemey 1")
-        Item.objects.create(text="itemey 2")
-        response = self.client.get("/")
-        self.assertContains(response, "itemey 1")
-        self.assertContains(response, "itemey 2")
 
     def test_can_save_a_POST_request(self):
         self.client.post("/", data={"item_text": "A new list item"})
@@ -24,11 +17,24 @@ class HomePage(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertRedirects(response, "/")
+        self.assertRedirects(response, "/lists/the-only-list-in-the-world/")
     
     def test_only_saves_items_when_neccessary(self):
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0)
+
+class ListViewTest(TestCase):
+    
+    def test_uses_list_template(self):
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertTemplateUsed(response, "list.html")
+    
+    def test_displays_all_list_items(self):
+        Item.objects.create(text="itemey 1")
+        Item.objects.create(text="itemey 2")
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertContains(response, "itemey 1")
+        self.assertContains(response, "itemey 2")
 
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
