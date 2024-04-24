380f5c4ccbf899c6e15b8ae730c333992bf96ef8
Wajih ElKaterji <wajih.elkaterji@wearelayer.com>
2024-04-24 19:51:42 +0300

    Added list item validation functional test

--- src/functional_tests/base.py
+++ src/functional_tests/base.py
@@ -32,3 +32,14 @@ class FunctionalTest(StaticLiveServerTestCase):
                     raise
 
                 time.sleep(0.5)
+    
+    def wait_for(self, fn):
+        start_time = time.time()
+        while True:
+            try:
+                return fn()
+            except (AssertionError, WebDriverException):
+                if (time.time() - start_time) > MAX_WAIT:
+                    raise
+
+                time.sleep(0.5)
--- src/functional_tests/test_list_item_validation.py
+++ src/functional_tests/test_list_item_validation.py
@@ -8,15 +8,35 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # Edith goes to the home page and accidentally tries to submit
         # an empty list item. She hits Enter on the empty input box
-
+        self.browser.get(self.live_server_url)
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
-
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
+                "You can't have an empty list item",  
+            )
+        )
+        
         # She tries again with some text for the item, which now works
+        self.browser.find_element(By.ID, "id_new_item").send_keys("Buy Milk")
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy Milk")
 
         # Perversely, she now decides to submit a second blank list item
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
 
         # She receives a similar warning on the list page
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element(By.CSS_SELECTOR, ".invalid-feedback").text,
+                "You can't have an empty list item",  
+            )
+        )
 
         # And she can correct it by filling some text in
-        self.fail("write me!")
+        self.browser.find_element(By.ID, "id_new_item").send_keys("Make tea")
+        self.browser.find_element(By.ID, "id_new_item").send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: Buy milk")
+        self.wait_for_row_in_list_table("2: Make tea")
